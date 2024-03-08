46ec3db23391f765e66a64941219e8fa69e6ec38
D3_83739_Pranav <pranavbobade03@gmail.com>
2024-03-08 17:01:37 +0530

    3rd question added

---
+++ Assignment_2/assignment2_Q3.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+
+namespace Nstudent{
+class Student{
+    
+    int prn;
+    string name;
+    int marks;
+
+public:
+    void initStudent()
+    {
+        prn=0;
+        name="NULL";
+        marks=0;
+    }
+    void acceptStudentFromConsole()
+    {
+        cout<<"Enter prn,name and marks";
+        cin>>prn>>name>>marks;
+    }
+    void printStudentOnConsole()
+    {
+        cout<<"prn : "<<prn<<"Name : "<<name<<"marks : "<<marks<<endl;
+    }
+};
+
+int menu()
+{
+    int choice;
+    cout<<"Enter 1 for default data"<<endl; 
+    cout<<"Enter 2 for enter your data"<<endl;
+    cin>>choice;
+    return choice;
+}
+}
+int main()
+{
+    Nstudent::Student s;
+    int ch;
+    while((ch=Nstudent::menu())!=0)
+    {
+        switch(ch)
+        {
+            case 1:
+            s.initStudent();
+            s.printStudentOnConsole();
+            break;
+
+            case 2:
+            s.acceptStudentFromConsole();
+            s.printStudentOnConsole();
+            break;
+
+            default:
+            cout<<"Wrong choice"<<endl;
+            break;
+        }
+    }
+    // s.printStudentOnConsole();
+    return 0;
+}
